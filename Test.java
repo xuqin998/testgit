0cb1d620a05027637b7508b5aee35097e3f1e77b
xuqin <cloud_xuqin@youzan.com>
2022-11-03 20:44:48 +0800

    修改了主分支有的代码

--- Test.java
+++ Test.java
@@ -1,6 +1,6 @@
 public class Test {
     public static void main(String[] args) {
-        System.out.println("hahaha");
+        System.out.println("hahaha-------");
         System.out.println("test2分支上的代码");
         System.out.println("看看这次合并master有的分支会不会变化");
         System.out.println("只有master分支有");
