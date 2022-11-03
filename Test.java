deefadebe6b8c80c1e86bdd3a951d948a55ab430
xuqin <cloud_xuqin@youzan.com>
2022-11-03 20:33:32 +0800

    只有master分支有的代码提交

--- Test.java
+++ Test.java
@@ -5,5 +5,6 @@ public class Test {
         System.out.println("test2分支上的代码");
         System.out.println("test2分支上的代码");
         System.out.println("test2分支上的代码");
+        System.out.println("只有master分支有");
     }
 }
