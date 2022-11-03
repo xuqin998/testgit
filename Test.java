38a494e23b5863c26a2d51e17c05fc7b8b152903
xuqin <cloud_xuqin@youzan.com>
2022-11-03 20:43:46 +0800

    分支3

--- Test.java
+++ Test.java
@@ -4,5 +4,6 @@ public class Test {
         System.out.println("test2分支上的代码");
         System.out.println("看看这次合并master有的分支会不会变化");
         System.out.println("只有master分支有");
+        System.out.println("分支3");
     }
 }
